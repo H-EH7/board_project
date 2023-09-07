464f7755c661bcfda32d0e74d4bf5c1a7474350d
H-EH7 <fly3767@gmail.com>
2023-09-07 18:27:47 +0900

    View 기본 형태

---
+++ src/main/java/eh7/board/controller/ViewController.java
@@ -0,0 +1,33 @@
+package eh7.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping
+    public String home() {
+        return "redirect:board";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/sign")
+    public String signPage() {
+        return "sign";
+    }
+
+    @GetMapping("/board")
+    public String boardPage() {
+        return "board";
+    }
+
+    @GetMapping("/post")
+    public String postPage() {
+        return "post";
+    }
+}
